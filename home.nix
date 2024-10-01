7eccc58d27450a07a474e4caad549d72eed1d7b1
albertvala <albert.vala@haxagon.cz>
2024-10-01 12:40:24 +0200

    fix: formatting

--- home.nix
+++ home.nix
@@ -1,6 +1,8 @@
-{ config, pkgs, ... }:
-
 {
+  config,
+  pkgs,
+  ...
+}: {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "albert";
@@ -19,16 +21,23 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
+    alejandra
+    fd
+    fzf
+    eww
+    lazygit
     gnupg
     pass
     libreoffice
     hyprpaper
     hyprlock
-    inkscape nodejs_22
+    inkscape
+    nodejs_22
     ferdium
     wev
     pfetch-rs
-    starship zoxide
+    starship
+    zoxide
     zsh
     adwaita-icon-theme
     morewaita-icon-theme
@@ -78,122 +87,129 @@
     localsend
     logseq
     git-credential-manager
-    cargo rustc haskellPackages.webkit2gtk3-javascriptcore
+    cargo
+    rustc
+    haskellPackages.webkit2gtk3-javascriptcore
     sbclPackages.cl-rsvg2
     android-studio
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
-    programs.zsh = {
-      enable = true;
-      enableCompletion = true;
-      autosuggestion.enable = true;
-      syntaxHighlighting.enable = true;
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
 
-      shellAliases = {
-        ll = "ls -l";
-        update = "sudo nixos-rebuild switch";
-      };
-      initExtra =''
-        pfetch
-        eval "$(zoxide init zsh)"
-        eval "$(starship init zsh)";
-      '';
-      history = {
-        size = 10000;
-      };
-
+    shellAliases = {
+      ll = "ls -l";
+      update = "sudo nixos-rebuild switch";
     };
-    programs.java.enable = true;
-      programs.git = {
-        enable = true;
-        userName  = "albertvala";
-        userEmail = "albert.vala@haxagon.cz";
-      };
-
-
-    wayland.windowManager.hyprland = {
-      enable = true;
-      xwayland.enable = true;
-      settings = {
-        env = [
-	  "HYPRCURSOR_THEME,GoogleDot-Black"
-	  "HYPRCURSOR_SIZE,24"
-	  "XCURSOR_THEME,GoogleDot-Black"
-	  "XCURSOR_SIZE,24"
-    "GCM_CREDENTIAL_STORE,gpg"
-    "NIXPKGS_ALLOW_INSECURE,1"
-    "GTK_THEME=Adwaita:dark"
-	];
-        exec-once = [
-	"asztal"
-	];
-        monitor = [
-	  ",preferred,auto,1"
-	];
-        general = {
-          gaps_in = 5;
-          gaps_out = 5;
-          border_size = 2;
-          "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
-          "col.inactive_border" = "rgba(595959aa)";
-        };
-        decoration = {
-          rounding = 5;
-        };
-        input = {
-          touchpad = {
-          };
-        };
-        gestures = {
-          workspace_swipe = true;
-          workspace_swipe_use_r = true;
-        };
-
-        "$mod" = "SUPER";
-         bind = [
-          # launch apps
-          "$mod, Q, exec, alacritty"
-          "$mod, F, exec, firefox-developer-edition"
-	  "$mod, SPACE, exec, nwg-drawer"
-          # kill active
-          "$mod, C, killactive,"
-          # movefocus
-          "$mod, left, movefocus, l"
-          "$mod, right, movefocus, r"
-          "$mod, up, movefocus, u"
-          "$mod, down, movefocus, d"
-	  "$mod, 1, workspace, 1"
-	  "$mod, 2, workspace, 2"
-	  "$mod, 3, workspace, 3"
-	  "$mod, 4, workspace, 4"
-	  "$mod, 5, workspace, 5"
-	  "$mod, 6, workspace, 6"
-	  "$mod, 7, workspace, 7"
-	  "$mod, 8, workspace, 8"
-	  "$mod, 9, workspace, 9"
-	  "$mod, 0, workspace, 10"
-	  "$mod SHIFT, 1, movetoworkspace, 1"
-	  "$mod SHIFT, 2, movetoworkspace, 2"
-	  "$mod SHIFT, 3, movetoworkspace, 3"
-	  "$mod SHIFT, 4, movetoworkspace, 4"
-	  "$mod SHIFT, 5, movetoworkspace, 5"
-	  "$mod SHIFT, 6, movetoworkspace, 6"
-	  "$mod SHIFT, 7, movetoworkspace, 7"
-	  "$mod SHIFT, 8, movetoworkspace, 8"
-	  "$mod SHIFT, 9, movetoworkspace, 9"
-	  "$mod SHIFT, 0, movetoworkspace, 10"
-    " , Print, exec, grim -g \"$(slurp -d)\" - | wl-copy"
-	  # window floating
-	  "$mod, V, togglefloating,"
-        ];
-	bindm = [
-	  "$mod, mouse:272, movewindow"
-	  "$mod, mouse:273, resizewindow"
-	];
-      };
+    initExtra = ''
+      pfetch
+      eval "$(zoxide init zsh)"
+      eval "$(starship init zsh)";
+    '';
+    history = {
+      size = 10000;
     };
+  };
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+  programs.java.enable = true;
+  programs.git = {
+    enable = true;
+    userName = "albertvala";
+    userEmail = "albert.vala@haxagon.cz";
+  };
+
+  wayland.windowManager.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+    settings = {
+      env = [
+        "HYPRCURSOR_THEME,GoogleDot-Black"
+        "HYPRCURSOR_SIZE,24"
+        "XCURSOR_THEME,GoogleDot-Black"
+        "XCURSOR_SIZE,24"
+        "GCM_CREDENTIAL_STORE,gpg"
+        "NIXPKGS_ALLOW_INSECURE,1"
+        "GTK_THEME=Adwaita:dark"
+      ];
+      exec-once = [
+        "eww open bar"
+      ];
+      monitor = [
+        ",preferred,auto,1"
+      ];
+      general = {
+        gaps_in = 5;
+        gaps_out = 5;
+        border_size = 2;
+        "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+        "col.inactive_border" = "rgba(595959aa)";
+      };
+      decoration = {
+        rounding = 5;
+      };
+      input = {
+        touchpad = {
+          "disable_while_typing" = false;
+        };
+      };
+      gestures = {
+        workspace_swipe = true;
+        workspace_swipe_use_r = true;
+        workspace_swipe_direction_lock = false;
+        workspace_swipe_forever = true;
+      };
+
+      "$mod" = "SUPER";
+      bind = [
+        # launch apps
+        "$mod, Q, exec, alacritty"
+        "$mod, F, exec, firefox-developer-edition"
+        "$mod, SPACE, exec, nwg-drawer"
+        # kill active
+        "$mod, C, killactive,"
+        # movefocus
+        "$mod, left, movefocus, l"
+        "$mod, right, movefocus, r"
+        "$mod, up, movefocus, u"
+        "$mod, down, movefocus, d"
+        "$mod, 1, workspace, 1"
+        "$mod, 2, workspace, 2"
+        "$mod, 3, workspace, 3"
+        "$mod, 4, workspace, 4"
+        "$mod, 5, workspace, 5"
+        "$mod, 6, workspace, 6"
+        "$mod, 7, workspace, 7"
+        "$mod, 8, workspace, 8"
+        "$mod, 9, workspace, 9"
+        "$mod, 0, workspace, 10"
+        "$mod SHIFT, 1, movetoworkspace, 1"
+        "$mod SHIFT, 2, movetoworkspace, 2"
+        "$mod SHIFT, 3, movetoworkspace, 3"
+        "$mod SHIFT, 4, movetoworkspace, 4"
+        "$mod SHIFT, 5, movetoworkspace, 5"
+        "$mod SHIFT, 6, movetoworkspace, 6"
+        "$mod SHIFT, 7, movetoworkspace, 7"
+        "$mod SHIFT, 8, movetoworkspace, 8"
+        "$mod SHIFT, 9, movetoworkspace, 9"
+        "$mod SHIFT, 0, movetoworkspace, 10"
+        " , Print, exec, grim -g \"$(slurp -d)\" - | wl-copy"
+        # window floating
+        "$mod, V, togglefloating,"
+      ];
+      bindm = [
+        "$mod, mouse:272, movewindow"
+        "$mod, mouse:273, resizewindow"
+      ];
+    };
+  };
 
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. These will be explicitly sourced when using a
